923e07b23bdc239b141de9cddbe65b25faf08f5a
liaoziyang <liaoziyang0405@gmail.com>
2017-09-08 00:13:05 +0900

    Add controler

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	void FixedUpdate(){
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.AddForce (x, 0, z);
+
+	}
+}
